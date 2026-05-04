9abe8f9158dd4dc90c94a158bdd050e86609d1bd
Almaz Galimov <almazwork@gmail.com>
2026-05-04 22:00:52 +0500

    исправлен вывод отпечатка

--- views/certs_templ.go
+++ views/certs_templ.go
@@ -53,15 +53,15 @@ func Certs(certs []model.Certificate) templ.Component {
 					return templ_7745c5c3_Err
 				}
 				var templ_7745c5c3_Var3 string
-				templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(cert.Thumbprint[:8])
+				templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(cert.Thumbprint)
 				if templ_7745c5c3_Err != nil {
-					return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/certs.templ`, Line: 20, Col: 67}
+					return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/certs.templ`, Line: 20, Col: 60}
 				}
 				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 				if templ_7745c5c3_Err != nil {
 					return templ_7745c5c3_Err
 				}
-				templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "...</td><td class=\"px-4 py-2\">")
+				templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "</td><td class=\"px-4 py-2\">")
 				if templ_7745c5c3_Err != nil {
 					return templ_7745c5c3_Err
 				}
